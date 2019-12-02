1fb79eadf6866d3128498e35927fb4bdb943fb36
pardeep <pardeep.pktapps@gmail.com>
2019-12-02 13:57:13 +0530

    cancel trade

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function LBCClient(key, secret, otp) {
 		let get_chat_sms  = "/";
 		let get_single_ad = "/";
 		let update_price_equation = "/";
+		let cancelTrade = "/"
 
 		if(params){
 			if(params.hasOwnProperty("ad_id")){
@@ -46,6 +47,7 @@ function LBCClient(key, secret, otp) {
 				params = null;
 				user_get_route = `contact_info/${id}`;
 				get_chat_sms = `contact_messages/${id}`;
+				cancelTrade = `contact_cancel/${id}`
 			}
 		}
 
@@ -56,7 +58,7 @@ function LBCClient(key, secret, otp) {
 			'dashboard/released/buyer', 'dashboard/canceled/buyer', 'dashboard/closed/buyer',
 			'dashboard/released/seller', 'dashboard/canceled/seller', 'dashboard/closed/seller',
 			'wallet-send', 'notifications', 'recent_messages', `${user_get_route}`,
-			`${post_chat_message}`, `${get_chat_sms}`, 'dashboard/canceled'
+			`${post_chat_message}`, `${get_chat_sms}`, 'dashboard/canceled', `${cancelTrade}`
 			]
 		};
 		
